5f61c2356d77d3afcea55fea0ec5cb8f15e1c1d9
vaadimches <moviedreamer12@gmail.com>
2026-06-11 20:59:47 +0300

    first setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 function App() {
   return (
     <>
+      <h1>Weather app</h1>
+      <input type="text" placeholder="input your city"/>
+      <button>search</button>
+      <h2>City</h2>
+      <p>temperature</p>
+      <p>Clear</p>
     </>
   )
 }
